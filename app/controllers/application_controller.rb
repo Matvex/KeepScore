7bddf84689b8fbe343e98cc0105d696ba1a34f3a
Matvex <em7575@gmail.com>
2025-05-15 22:58:13 +0300

    Feature/mark-active-tab (#28)
    * Mark active tab
    
    * fixup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   before_action :set_locale
-  
+
   allow_browser versions: :modern
 
   helper_method :current_user, :user_signed_in?
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -13,4 +13,16 @@ document.addEventListener("turbo:load", function() {
       Turbo.visit(url.toString());
     });
   }
+
+  const links = document.querySelectorAll(".nav-link");
+  const currentPath = window.location.pathname;
+
+  links.forEach(link => {
+    const linkPath = new URL(link.href).pathname;
+    if (currentPath.startsWith(linkPath)) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
 });
