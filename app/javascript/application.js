8282a75793ab8722b516818f85b954b95cbe217b
Elvis Matveks <em7575@gmail.com>
2025-05-23 15:29:33 +0300

    Convert all javascript to stimulus

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,28 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-
-document.addEventListener("turbo:load", function() {
-  const languageSwitch = document.getElementById("language-switch");
-
-  if (languageSwitch) {
-    languageSwitch.addEventListener("change", function() {
-      const form = document.getElementById("language-switch-form");
-      const url = new URL(form.action);
-      url.searchParams.set("locale", languageSwitch.checked ? "lv" : "en");
-      Turbo.visit(url.toString());
-    });
-  }
-
-  const links = document.querySelectorAll(".nav-link");
-  const currentPath = window.location.pathname;
-
-  links.forEach(link => {
-    const linkPath = new URL(link.href).pathname;
-    if (currentPath.startsWith(linkPath)) {
-      link.classList.add("active");
-    } else {
-      link.classList.remove("active");
-    }
-  });
-});
